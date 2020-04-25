4155bfa55cb86da0fbe9a28491a3f2cc16bae2b2
Eugenia <evg3307@yandex.ru>
2020-04-25 22:32:07 +0300

    refactor, step 1

--- knapsack_my_test_numpy_vs_list.py
+++ knapsack_my_test_numpy_vs_list.py
@@ -3,23 +3,25 @@ import sys
 import numpy as np
 from time import time
 
+
 def optimal_weight(capacity, items_list):
     items_num = len (items_list)
-    dp_table = [[0]*(capacity+1) for _ in range (items_num+1)]
+    dp_table = [[0]*(capacity + 1) for _ in range(items_num + 1)]
 
     for i in range(1, items_num + 1):
         for j in range(1, capacity + 1):
-            prev_table_weight = dp_table[i-1][j]
-            prev_item_weight = items_list[i-1]
+            prev_table_weight = dp_table[i - 1][j]
+            prev_item_weight = items_list[i - 1]
 
             if prev_item_weight > j:
                 dp_table[i][j] = prev_table_weight
             else:
                 dp_table[i][j] = max(
-                    dp_table[i-1][j - prev_item_weight] + prev_item_weight,
+                    dp_table[i - 1][j - prev_item_weight] + prev_item_weight,
                     prev_table_weight
                 )
     return dp_table[-1][-1]
+
 
 def optimal_weight_numpy(capacity, items_list):
     items_num = len(items_list)
@@ -27,14 +29,14 @@ def optimal_weight_numpy(capacity, items_list):
 
     for i in range(1, items_num + 1):
         for j in range(1, capacity + 1):
-            prev_table_weight = dp_table[i-1][j]
-            prev_item_weight = items_list[i-1]
+            prev_table_weight = dp_table[i - 1][j]
+            prev_item_weight = items_list[i - 1]
 
             if prev_item_weight > j:
                 dp_table[i][j] = prev_table_weight
             else:
                 dp_table[i][j] = max(
-                    dp_table[i-1][j - prev_item_weight] + prev_item_weight,
+                    dp_table[i - 1][j - prev_item_weight] + prev_item_weight,
                     prev_table_weight
                 )
     return dp_table[-1][-1]
