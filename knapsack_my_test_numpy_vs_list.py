7faf24b1f7e00f2883c98fa36c0f059740c2fb49
Eugenia <evg3307@yandex.ru>
2020-04-25 22:38:19 +0300

    refactor, step 3

--- knapsack_my_test_numpy_vs_list.py
+++ knapsack_my_test_numpy_vs_list.py
@@ -25,7 +25,7 @@ def optimal_weight(capacity, items_list):
 
 def optimal_weight_numpy(capacity, items_list):
     items_num = len(items_list)
-    dp_table = np.zeros((items_num + 1, capacity + 1), dtype = np.int16)
+    dp_table = np.zeros((items_num + 1, capacity + 1), dtype=np.int16)
 
     for i in range(1, items_num + 1):
         for j in range(1, capacity + 1):
@@ -46,11 +46,14 @@ def optimal_weight_numpy(capacity, items_list):
 W = 100
 w = list(range(100000))
 start_time = time()
-_ = optimal_weight(W, w)
+res_list = optimal_weight(W, w)
 print(f"list -  time sec: {time() - start_time}")
 
 start_time = time()
-_ = optimal_weight_numpy(W, w)
+res_numpy = optimal_weight_numpy(W, w)
 print(f"numpy - time sec {time() - start_time}")
+
+if res_numpy != res_list:
+    print("A ya tomat")
 
 
